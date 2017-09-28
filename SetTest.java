74619bcafb75772fd32ef0ddd64e895b3670b599
M3rrowY3rrow <32042932+M3rrowY3rrow@users.noreply.github.com>
2017-09-28 00:08:41 -0400

    Update SetTest.java

--- SetTest.java
+++ SetTest.java
@@ -65,5 +65,162 @@ public abstract class SetTest {
     }
 
     // TODO - add test cases for constructor, add, remove, removeAny, contains, and size
+    @Test
+    public final void testNoArgumentConstructor() {
+        /*
+         * Set up variables and call method under test
+         */
+        Set<String> q = this.constructorTest();
+        Set<String> qExpected = this.constructorRef();
+        /*
+         * Assert that values of variables match expectations
+         */
+        assertEquals(qExpected, q);
+    }
 
+    @Test
+    public final void testRemoveToEmpty() {
+        /*
+         * Set up variables and call method under test
+         */
+        Set<String> q = this.createFromArgsTest("green");
+        Set<String> qExpected = this.createFromArgsRef();
+        /*
+         * Call method under test
+         */
+        String trash = q.remove("green");
+        /*
+         * Assert that values of variables match expectations
+         */
+        assertEquals(qExpected, q);
+    }
+
+    @Test
+    public final void testRemoveFromSize5() {
+        /*
+         * Set up variables and call method under test
+         */
+        Set<String> q = this.createFromArgsTest("green", "red", "white", "blue",
+                "purple");
+        Set<String> qExpected = this.createFromArgsRef("red", "white", "blue",
+                "purple");
+        /*
+         * Call method under test
+         */
+        String trash = q.remove("green");
+        /*
+         * Assert that values of variables match expectations
+         */
+        assertEquals(qExpected, q);
+    }
+
+    @Test
+    public final void testRemoveFromSize15() {
+        /*
+         * Set up variables and call method under test
+         */
+        Set<String> q = this.createFromArgsTest("green", "red", "white", "blue",
+                "purple", "orange", "teal", "indigo", "yellow", "black",
+                "brown", "pink", "scarlet", "violet", "grey");
+        Set<String> qExpected = this.createFromArgsRef("red", "white", "blue",
+                "purple", "orange", "teal", "indigo", "yellow", "black",
+                "brown", "pink", "scarlet", "violet", "grey");
+        /*
+         * Call method under test
+         */
+        String trash = q.remove("green");
+        /*
+         * Assert that values of variables match expectations
+         */
+        assertEquals(qExpected, q);
+    }
+
+    @Test
+    public final void testContainsTrueSize15() {
+        /*
+         * Set up variables and call method under test
+         */
+        Set<String> q = this.createFromArgsTest("green", "red", "white", "blue",
+                "purple", "orange", "teal", "indigo", "yellow", "black",
+                "brown", "pink", "scarlet", "violet", "grey");
+        Set<String> qExpected = this.createFromArgsRef("green", "red", "white",
+                "blue", "purple", "orange", "teal", "indigo", "yellow", "black",
+                "brown", "pink", "scarlet", "violet", "grey");
+        boolean in = false;
+        /*
+         * Call method under test
+         */
+        in = q.contains("green");
+        /*
+         * Assert that values of variables match expectations
+         */
+        assertEquals(qExpected, q);
+        assertEquals(in, true);
+    }
+
+    @Test
+    public final void testContainsFalseSize15() {
+        /*
+         * Set up variables and call method under test
+         */
+        Set<String> q = this.createFromArgsTest("green", "red", "white", "blue",
+                "purple", "orange", "teal", "indigo", "yellow", "black",
+                "brown", "pink", "scarlet", "violet", "grey");
+        Set<String> qExpected = this.createFromArgsRef("green", "red", "white",
+                "blue", "purple", "orange", "teal", "indigo", "yellow", "black",
+                "brown", "pink", "scarlet", "violet", "grey");
+        boolean in = false;
+        /*
+         * Call method under test
+         */
+        in = q.contains("emerald");
+        /*
+         * Assert that values of variables match expectations
+         */
+        assertEquals(qExpected, q);
+        assertEquals(in, false);
+    }
+
+    @Test
+    public final void testContainsTrueSize5() {
+        /*
+         * Set up variables and call method under test
+         */
+        Set<String> q = this.createFromArgsTest("green", "red", "white", "blue",
+                "purple");
+        Set<String> qExpected = this.createFromArgsRef("green", "red", "white",
+                "blue", "purple");
+        boolean in = false;
+        /*
+         * Call method under test
+         */
+        in = q.contains("green");
+        /*
+         * Assert that values of variables match expectations
+         */
+        assertEquals(qExpected, q);
+        assertEquals(in, true);
+    }
+
+    @Test
+    public final void testContainsFalseSize5() {
+        /*
+         * Set up variables and call method under test
+         */
+        Set<String> q = this.createFromArgsTest("green", "red", "white", "blue",
+                "purple");
+        Set<String> qExpected = this.createFromArgsRef("green", "red", "white",
+                "blue", "purple");
+        boolean in = false;
+        /*
+         * Call method under test
+         */
+        in = q.contains("ruby");
+        /*
+         * Assert that values of variables match expectations
+         */
+        assertEquals(qExpected, q);
+        assertEquals(in, false);
+    }
 }
+
